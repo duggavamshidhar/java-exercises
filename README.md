caf267f04808fb82caf9f092642fe83fde8d38dd
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-05-12 20:44:16 +0530

    Exercise 5

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 2. [Java Program to Convert Long Values to Bytes](src/com/duggavamshidhar/ExerciseTwo.java)
 3. [Java Program to Illustrate the Use of Binary Literals](src/com/duggavamshidhar/ExerciseThree.java)
 4. [Java Program to Use Underscores in Numeric Literals](src/com/duggavamshidhar/ExerciseFour.java)
+5. [Java Program to Perform Relational Operations](src/com/duggavamshidhar/ExerciseFive.java)
---
+++ src/com/duggavamshidhar/ExerciseFive.java
@@ -0,0 +1,22 @@
+package com.duggavamshidhar;
+
+import java.util.Scanner;
+
+// Java Program to Perform Relational Operations
+public class ExerciseFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        System.out.println("Enter first number: ");
+        a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        b = sc.nextInt();
+        System.out.println("First number is greater than second number: " + (a > b));
+        System.out.println("First number is less than second number: " + (a < b));
+        System.out.println("First number is equal to second number: " + (a == b));
+        System.out.println("First number is not equal to second number: " + (a != b));
+        System.out.println("First number is greater or equal to second number: " + (a > b));
+        System.out.println("First number is lesser or equal to second number: " + (a < b));
+    }
+}
