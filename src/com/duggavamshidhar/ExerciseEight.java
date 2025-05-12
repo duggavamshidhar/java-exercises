7026b2210cd0a6fb74a180386da456ddee2e2834
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-05-12 21:18:10 +0530

    Update: Exercise 8

--- src/com/duggavamshidhar/ExerciseEight.java
+++ src/com/duggavamshidhar/ExerciseEight.java
@@ -1,11 +1,8 @@
 package com.duggavamshidhar;
 
-import java.util.Scanner;
-
 // Java Program to Illustrate the Use of Pre- and Post-Increment and Decrement Operators
 public class ExerciseEight {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int a = 10, b = 10, c = 10, d = 10;
         System.out.println("Pre-Increment: " + ++a);
         System.out.println("Post-Increment: " + b++);
